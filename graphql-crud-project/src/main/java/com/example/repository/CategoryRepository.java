87e9f744d0ea72d6ffb6ffcd09dd2148a7637239
thanhtanmt <tanle20022005@gmail.com>
2025-10-06 17:22:48 +0700

    Delete graphql-crud-project directory

--- graphql-crud-project/src/main/java/com/example/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
